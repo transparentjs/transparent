055635162777410ce104d09dd328103d17694060
Tim Shadel <tim@shadelsoftware.com>
2013-02-27 17:36:26 -0700

    Add basic tests for objects.

--- test/transparent.test.js
+++ test/transparent.test.js
@@ -19,4 +19,22 @@ describe("transparent", function() {
 
   });
 
+  describe(".proxy(object, 'method')", function() {
+    var object = {};
+    var method = function method() { return 42; };
+    object['method'] = method;
+    transparent.proxy(object, 'method');
+
+    it("should wrap 'method' with a proxy", function() {
+      object['method'].should.not.equal(method);
+    });
+
+    it("should return the same result as 'method'", function() {
+      var methodResult = method.apply(object);
+      var wrappedResult = object.method();
+      wrappedResult.should.equal(methodResult);
+    });
+
+  });
+
 });
